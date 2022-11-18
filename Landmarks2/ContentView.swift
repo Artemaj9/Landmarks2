5237b0b7a68cf6ed291ff6f609d26d48229805e9
Artem <moiseev@physics.msu.ru>
2022-11-18 21:17:33 +0300

    Initial Commit

---
+++ Landmarks2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks2
+//
+//  Created by Artem on 18.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks2/Landmarks2App.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks2App.swift
+//  Landmarks2
+//
+//  Created by Artem on 18.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
